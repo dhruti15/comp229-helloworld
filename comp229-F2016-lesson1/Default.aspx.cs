4ae8e29efef68cd4619ce91485a47da5c75dd25b
Dhrutiben Patel <dpate483@my.centennialcollege.ca>
2016-09-08 14:04:04 +0100

    final changes

--- comp229-F2016-lesson1/Default.aspx.cs
+++ comp229-F2016-lesson1/Default.aspx.cs
@@ -9,9 +9,12 @@ namespace comp229_F2016_lesson1
 {
     public partial class _Default : Page
     {
+        ///<summary>
+        ///this method executes every time the page loads
+        ///</summary>
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            HelloLabel.Text = "Good Bye!!";
         }
     }
 }
